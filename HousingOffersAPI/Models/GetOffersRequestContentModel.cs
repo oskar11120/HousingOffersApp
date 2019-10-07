caf8434154e08c2b13e5ec52da41fa557daf3c9d
oskarfurlepa <oskarfurlepa@interia.pl>
2019-10-07 14:18:21 +0200

    Added models.

---
+++ HousingOffersAPI/Models/GetOffersRequestContentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingOffersAPI.Models
+{
+    public class GetOffersRequestContentModel
+    {
+        public double[] PriceLimits { get; set; }
+        public double[] AreaLimits { get; set; }
+        public string[] PropertuTypes { get; set; }
+        public string[] OfferTypes { get; set; }
+        public string OfferId { get; set; }
+        public string UserId { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ HousingOffersAPI/Models/OfferModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingOffersAPI.Models
+{
+    public class OfferModel
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public double PriceInPLN { get; set; }
+        public string Adress { get; set; }
+        public double Area { get; set; }
+        public string[] ProperyTypes { get; set; }
+        public string[] OfferTypes { get; set; }
+        public string ActionType { get; set; }
+        public Dictionary<string, string> OfferTags { get; set; }
+        public string Description { get; set; }
+        public string Images { get; set; }
+    }
+}
