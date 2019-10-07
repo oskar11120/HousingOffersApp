54f4cff45127d2940c205b372c34a3f475c56f55
oskarfurlepa <oskarfurlepa@interia.pl>
2019-10-07 13:57:42 +0200

    Created repo.

---
+++ HousingOffersAPI/Controllers/DataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingOffersAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        // returns offers for given getOffersInput
+        [HttpPost("getOffers")]
+        public void GetOffers([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // adds new offer to database
+        [HttpPost("newOffer")]
+        public void AddOffer([FromBody] string createOfferInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        // deletes off that correspons to given id
+        [HttpDelete("offerId")]
+        public void DeleteOffer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HousingOffersAPI/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingOffersAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [Route("login")]
+        public void Login([FromBody] string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("register")]
+        public void Register([FromBody] string user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
